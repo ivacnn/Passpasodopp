75a71cc830d5d5684ab2773f4e584f90fb9e27ad
ivacnn <ip828157@gmail.com>
2024-11-09 17:33:58 +0500

    uyttyrr
    wesrq

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //эта команда выведет текст.
+        print("Hello, World!");     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
